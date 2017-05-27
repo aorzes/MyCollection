70deb67f5311e3f4ef2da437c82b11658431a425
Anton Orzes <antonorzes@antons-mac-mini.local>
2017-05-27 12:39:54 +0200

    Initial Commit

---
+++ CollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionView
+//
+//  Created by Anton Orzes on 27/05/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionView
+//
+//  Created by Anton Orzes on 27/05/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
